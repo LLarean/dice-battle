e61700813ec7577c37f13d8a887b551c17f12414
Stanislav Kozlov <llarean@ya.ru>
2025-11-14 18:50:21 +0300

    Updating gitignore, working on DiceType.cs

--- Assets/_DiceBattle/Scripts/Core/DiceType.cs
+++ Assets/_DiceBattle/Scripts/Core/DiceType.cs
@@ -1,16 +1,28 @@
-using UnityEngine;
-
-public class DiceType : MonoBehaviour
+namespace DiceBattle.Core
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Represents the different types of dice used in the game.
+    /// </summary>
+    public enum DiceType
     {
-        
-    }
+        /// <summary>
+        /// Represents an empty dice type in the game.
+        /// </summary>
+        Empty = 0,
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Represents a dice type used to perform offensive actions in the game.
+        /// </summary>
+        Attack = 1,
+
+        /// <summary>
+        /// Represents a dice type focused on defensive actions, such as reducing damage or enhancing protection in the game.
+        /// </summary>
+        Defense = 2,
+
+        /// <summary>
+        /// Represents a dice type used to restore health points in the game.
+        /// </summary>
+        Heal = 3,
     }
 }
