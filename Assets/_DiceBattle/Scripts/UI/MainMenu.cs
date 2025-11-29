33b1192f724a3338a284f2cff05f9e94dd26cc0f
Stanislav Kozlov <llarean@ya.ru>
2025-11-29 04:47:58 +0300

    Adding the main screen

---
+++ Assets/_DiceBattle/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+using DiceBattle.Screens;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DiceBattle
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _start;
+        [SerializeField] private GameScreen _gameScreen;
+
+        private void Start() => _start.onClick.AddListener(StartClick);
+
+        private void OnDestroy() => _start.onClick.RemoveAllListeners();
+
+        private void StartClick()
+        {
+            _gameScreen.gameObject.SetActive(true);
+        }
+    }
+}
