03ff0c2cc1d8adcb239b91114280decd3a1e1bdf
Stanislav Kozlov <llarean@ya.ru>
2026-01-22 15:32:27 +0300

    Inventory
    Refactoring.
    Recycling the inventory window.
    Updating the inventory display logic.

--- Assets/_DiceBattle/Scripts/UI/Components/InventoryItem.cs
+++ Assets/_DiceBattle/Scripts/UI/Components/InventoryItem.cs
@@ -1,11 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 namespace DiceBattle.UI
 {
     public class InventoryItem : MonoBehaviour
     {
-        [SerializeField] private RewardType _rewardType;
+        [SerializeField] private TextMeshProUGUI _title;
+        private RewardType _rewardType;
 
         public RewardType RewardType => _rewardType;
+
+        public void Construct(RewardType rewardType)
+        {
+            _rewardType = rewardType;
+            // TODO Add title display and translation into different languages
+            _title.text = rewardType.ToString();
+        }
     }
 }
--- Assets/_DiceBattle/Scripts/UI/Windows/InventoryWindow.cs
+++ Assets/_DiceBattle/Scripts/UI/Windows/InventoryWindow.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections.Generic;
-using DiceBattle.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,74 +7,66 @@ namespace DiceBattle.UI
 {
     public class InventoryWindow : Screen
     {
+        private readonly List<InventoryItem> _items = new();
+
+        [Space]
         [SerializeField] private Transform _substrate;
         [SerializeField] private Button _close;
         [Space]
-        [SerializeField] private List<Dice> _dices;
-        [SerializeField] private List<InventoryItem> _items;
+        [SerializeField] private InventoryItem _item;
+        [SerializeField] private Transform _itemsSpawn;
 
         private void Start()
         {
             _close.onClick.AddListener(HandleCloseClick);
-
-            foreach (Dice dice in _dices)
-            {
-                dice.DisableButton();
-            }
-
-            gameObject.SetActive(false);
+            GenerateItems();
         }
 
         private void OnDestroy() => _close.onClick.RemoveAllListeners();
 
-        private void OnEnable()
-        {
-            EnableDice();
-            EnableItems();
-        }
+        private void OnEnable() => ToggleItems();
 
         private void HandleCloseClick() => gameObject.SetActive(false);
 
-        private void EnableDice()
+        private void GenerateItems()
         {
-            int availableDice = PlayerPrefs.GetInt("AvailableDice", 3);
+            ClearItems();
 
-            for (int i = 0; i < _dices.Count; i++)
+            foreach (RewardType rewardType in Enum.GetValues(typeof(RewardType)))
             {
-                _dices[i].gameObject.SetActive(i < availableDice);
+                InventoryItem inventoryItem = Instantiate(_item, _itemsSpawn);
+                inventoryItem.Construct(rewardType);
+                _items.Add(inventoryItem);
             }
         }
 
-        private void EnableItems()
+        private void ClearItems()
         {
-            ToggleRewardItem(RewardType.DisableEmptyState);
-            ToggleRewardItem(RewardType.BaseDamage);
-            ToggleRewardItem(RewardType.RegenHealth);
-            ToggleRewardItem(RewardType.BaseArmor);
+            foreach (InventoryItem inventoryItem in _items)
+            {
+                Destroy(inventoryItem.gameObject);
+            }
+
+            _items.Clear();
         }
 
-        private void ToggleRewardItem(RewardType rewardType)
+        private void ToggleItems()
         {
-            if (TryGetRewardItem(rewardType, out InventoryItem item))
+            foreach (RewardType rewardType in Enum.GetValues(typeof(RewardType)))
             {
-                item.gameObject.SetActive(PlayerPrefs.GetInt(nameof(rewardType), 0) == 1);
+                ToggleItem(rewardType);
             }
         }
 
-        private bool TryGetRewardItem(RewardType rewardType, out InventoryItem inventoryItem)
+        private void ToggleItem(RewardType rewardType)
         {
-            inventoryItem = null;
-
             foreach (InventoryItem item in _items)
             {
                 if (item.RewardType == rewardType)
                 {
-                    inventoryItem = item;
-                    return true;
+                    item.gameObject.SetActive(PlayerPrefs.GetInt(nameof(rewardType), 0) == 1);
                 }
             }
-
-            return false;
         }
     }
 }
