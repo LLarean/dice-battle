6a68e53b08209042b96ffcdf4e4fc69472c58961
Stanislav Kozlov <llarean@ya.ru>
2026-01-23 13:51:13 +0300

    Added the "Credits" button

--- Assets/_DiceBattle/Scripts/UI/Windows/OptionsWindow.cs
+++ Assets/_DiceBattle/Scripts/UI/Windows/OptionsWindow.cs
@@ -9,22 +9,28 @@ namespace DiceBattle.UI
     public class OptionsWindow : Screen
     {
         [Space]
-        [SerializeField] private Button _close;
         [SerializeField] private Slider _music;
         [SerializeField] private Slider _sound;
+        [Space]
+        [SerializeField] private Button _credits;
+        [SerializeField] private Button _close;
 
         private void Start()
         {
-            _close.onClick.AddListener(HandleCloseClick);
             _music.onValueChanged.AddListener(HandleMusicChange);
             _sound.onValueChanged.AddListener(HandleSoundChange);
+
+            _credits.onClick.AddListener(HandleCreditsClick);
+            _close.onClick.AddListener(HandleCloseClick);
         }
 
         private void OnDestroy()
         {
-            _close.onClick.RemoveAllListeners();
             _music.onValueChanged.RemoveAllListeners();
             _sound.onValueChanged.RemoveAllListeners();
+
+            _credits.onClick.RemoveAllListeners();
+            _close.onClick.RemoveAllListeners();
         }
 
         private void OnEnable()
@@ -32,8 +38,6 @@ namespace DiceBattle.UI
             _music.value = GameSettings.MusicVolume;
             _sound.value = GameSettings.SoundVolume;
         }
-
-        private void HandleCloseClick() => gameObject.SetActive(false);
 
         private void HandleMusicChange(float musicValue)
         {
@@ -44,5 +48,13 @@ namespace DiceBattle.UI
         {
             SignalSystem.Raise<ISoundHandler>(handler => handler.SetSoundValue(soundValue));
         }
+
+        private void HandleCreditsClick()
+        {
+            Debug.Log("Credits");
+            gameObject.SetActive(false);
+        }
+
+        private void HandleCloseClick() => gameObject.SetActive(false);
     }
 }
