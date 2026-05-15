007a7b0a95fa82fdc54f455a516513ad149e6874
Kozlov Stanislav <llarean@ya.ru>
2026-05-15 08:49:22 +0300

    Minor fixes

--- Assets/_DiceBattle/Scripts/Core/Spawner.cs
+++ Assets/_DiceBattle/Scripts/Core/Spawner.cs
@@ -2,6 +2,7 @@
 using DiceBattle.Data;
 using DiceBattle.Global;
 using DiceBattle.UI;
+using UnityEngine;
 
 namespace DiceBattle.Core
 {
@@ -51,15 +52,24 @@ namespace DiceBattle.Core
         {
             DiceList diceList = GameData.GetInventory();
 
-            int doubleHealthCount = diceList.DiceTypes.Count(r => r == DiceBattle.DiceType.BaseHealth);
+            int doubleHealthCount = diceList.DiceTypes.Count(r => r == DiceType.BaseHealth);
             int additionalHealth = unitConfig.StartHealth * doubleHealthCount;
-            int maxHealth = unitConfig.StartHealth + additionalHealth;
+            int maxHealth = unitConfig.StartHealth * doubleHealthCount;
+            // int maxHealth = unitConfig.StartHealth + additionalHealth;
 
-            int baseDamageCount = diceList.DiceTypes.Count(r => r == DiceBattle.DiceType.BaseDamage);
+            // Debug.Log("diceList.DiceTypes.Count(r => r == DiceType.BaseHealth) = " + diceList.DiceTypes.Count(r => r == DiceType.BaseHealth));
+            // Debug.Log("additionalHealth = " + additionalHealth);
+            // Debug.Log("maxHealth = " + maxHealth);
+            // Debug.Log("unitConfig.StartHealth = " + unitConfig.StartHealth);
+
+            int baseDamageCount = diceList.DiceTypes.Count(r => r == DiceType.BaseDamage);
             int damage = unitConfig.StartDamage + baseDamageCount;
 
-            int baseArmorCount = diceList.DiceTypes.Count(r => r == DiceBattle.DiceType.BaseArmor);
+            int baseArmorCount = diceList.DiceTypes.Count(r => r == DiceType.BaseArmor);
             int armor = unitConfig.StartArmor + baseArmorCount;
+
+            // Debug.Log("diceList.DiceTypes.Count(r => r == DiceType.BaseDamage) = " + diceList.DiceTypes.Count(r => r == DiceType.BaseDamage));
+            // Debug.Log("diceList.DiceTypes.Count(r => r == DiceType.BaseArmor) = " + diceList.DiceTypes.Count(r => r == DiceType.BaseArmor));
 
             return new UnitData
             {
