f8d10ee47f1bbf8c3cd57fb2c2f6c6013be0e112
Kozlov Stanislav <llarean@ya.ru>
2026-08-09 07:37:46 +0300

    Add localization initializer and LocalizedTMP
    Introduce localization support: add Assets/_DiceBattle/Scripts/Localization with LocalizationInitializer (auto-sets LocalizationManager.Language at startup) and LocalizedTMP component to localize TextMeshPro text. Include corresponding Unity .meta files. Update .editorconfig to add naming rules for const and static readonly fields, renumber Unity naming rules, and adjust symbol definitions.

---
+++ Assets/_DiceBattle/Scripts/Localization/LocalizedTMP.cs
@@ -0,0 +1,38 @@
+﻿using Assets.SimpleLocalization.Scripts;
+using TMPro;
+using UnityEngine;
+
+namespace DiceBattle.Localization
+{
+    /// <summary>
+    /// Localize TMP_Text component.
+    /// </summary>
+    [RequireComponent(typeof(TMP_Text))]
+    public class LocalizedTMP : MonoBehaviour
+    {
+        [SerializeField] private string _localizationKey;
+
+        private TMP_Text _text;
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Start()
+        {
+            Localize();
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        private void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            _text.text = LocalizationManager.Localize(_localizationKey);
+        }
+    }
+}
